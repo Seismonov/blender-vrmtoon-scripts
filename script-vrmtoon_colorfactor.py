0177446dad49b5b318aea6be605672e31bff18f0
Pertiwang Sismananda <iwangmps@gmail.com>
2024-06-06 20:10:48 +0700

    Updated behavior

--- script-vrmtoon_colorfactor.py
+++ script-vrmtoon_colorfactor.py
@@ -4,12 +4,13 @@ import bpy
 # must be 4 input
 base_color_factor = [1.000000, 1.000000, 1.000000, 1.000000]
 # must be 3 input
-shade_color_factor = [0.195962, 0.195962, 0.195962]
+shade_color_factor = [0.500000, 0.500000, 0.500000]
 
 #index = 0
 #object = bpy.context.view_layer.objects.active
 
 for material in bpy.data.materials:
-    if material.vrm_addon_extension.mtoon1.enabled == True:
-        material.vrm_addon_extension.mtoon1.pbr_metallic_roughness.base_color_factor = base_color_factor
-        material.vrm_addon_extension.mtoon1.extensions.vrmc_materials_mtoon.shade_color_factor = shade_color_factor
+    if material.vrm_addon_extension.mtoon1.enabled:
+        if material.vrm_addon_extension.mtoon1.pbr_metallic_roughness.base_color_texture.index.source != None and material.vrm_addon_extension.mtoon1.extensions.vrmc_materials_mtoon.shade_multiply_texture.index.source != None:
+            material.vrm_addon_extension.mtoon1.pbr_metallic_roughness.base_color_factor = base_color_factor
+            material.vrm_addon_extension.mtoon1.extensions.vrmc_materials_mtoon.shade_color_factor = shade_color_factor
